119783e7e843b12b4058e3c8be4cd241fa3fef83
rattletone <rattletone@protonmail.com>
2019-03-11 19:51:57 +0000

    Added Heightmap class
    - heightmap.js create() is now static factory method of Heightmap class
    - Heightmap class is now default export of heightmap.js

--- src/heightmap.js
+++ src/heightmap.js
@@ -44,94 +44,96 @@ function* random(aSeed, bSeed, cSeed, dSeed) {
 	}
 }
 
-function create(iterations, seed) {
-	if(isNaN(Number(iterations))) {
-		throw new TypeError("\"iterations\" must be of type, or able to be converted to type, \"number\"");
-	}
-
-	const hashSeed = typeof seed === "string" ? seed : Math.random().toString(36);
-	const randomSeed = hash(hashSeed);
-	const randomNumber = random(randomSeed.next().value, randomSeed.next().value, randomSeed.next().value, randomSeed.next().value);
-
-	function noise(strength) {
-		return ((randomNumber.next().value * (1 - -1)) + -1) * strength;
-	}
-
-	const ubound = Math.pow(2, iterations);
-	const heightmap = [];
-	let i = ubound + 1;
-
-	while(i--) {
-		heightmap[i] = [];
-	}
-
-	heightmap[0][0] = randomNumber.next().value;
-	heightmap[0][ubound] = randomNumber.next().value;
-	heightmap[ubound][0] = randomNumber.next().value;
-	heightmap[ubound][ubound] = randomNumber.next().value;
-
-	let div = ubound;
-	let noiseStrength = 1;
-
-	while(div > 1) {
-		for(let y = 0; y < ubound; y += div) {
-			for(let x = 0; x < ubound; x += div) {
-				const a = heightmap[y][x];
-				const b = heightmap[y][x + div];
-				const c = heightmap[y + div][x];
-				const d = heightmap[y + div][x + div];
-
-				heightmap[y + (div / 2)][x + (div / 2)] = clamp(((a + b + c + d) / 4) + noise(noiseStrength));
-			}
+class Heightmap {
+	static create(iterations, seed) {
+		if(isNaN(Number(iterations))) {
+			throw new TypeError("\"iterations\" must be of type, or able to be converted to type, \"number\"");
 		}
 
-		for(let y = 0; y < ubound; y += div) {
-			for(let x = 0; x < ubound; x += div) {
-				const a = heightmap[y][x];
-				const b = heightmap[y][x + div];
-				const c = heightmap[y + div][x];
-				const d = heightmap[y + div][x + div];
-				const e = heightmap[y + (div / 2)][x + (div / 2)];
+		const hashSeed = typeof seed === "string" ? seed : Math.random().toString(36);
+		const randomSeed = hash(hashSeed);
+		const randomNumber = random(randomSeed.next().value, randomSeed.next().value, randomSeed.next().value, randomSeed.next().value);
 
-				heightmap[y][x + (div / 2)] = clamp(
-					(y
-						? (heightmap[y - (div / 2)][x + (div / 2)] + a + b + e) / 4
-						: (a + b + e) / 3
-					)
-					+ noise(noiseStrength)
-				);
-
-				heightmap[y + (div / 2)][x] = clamp(
-					(x
-						? (heightmap[y + (div / 2)][x - (div / 2)] + a + e + c) / 4
-						: (a + e + c) / 3
-					)
-					+ noise(noiseStrength)
-				);
-
-				heightmap[y + (div / 2)][x + div] = clamp(
-					(x + div < ubound
-						? (heightmap[y + (div / 2)][x + div + (div / 2)] + b + e + d) / 4
-						: (b + e + d) / 3
-					)
-					+ noise(noiseStrength)
-				);
-
-				heightmap[y + div][x + (div / 2)] = clamp(
-					(y + div < ubound
-						? (heightmap[y + div + (div / 2)][x + (div / 2)] + e + c + d) / 4
-						: (e + c + d) / 3
-					)
-					+ noise(noiseStrength)
-				);
-			}
+		function noise(strength) {
+			return ((randomNumber.next().value * (1 - -1)) + -1) * strength;
 		}
 
-		div /= 2;
-		noiseStrength /= 2;
-	}
+		const ubound = Math.pow(2, iterations);
+		const heightmap = [];
+		let i = ubound + 1;
 
-	return heightmap;
+		while(i--) {
+			heightmap[i] = [];
+		}
+
+		heightmap[0][0] = randomNumber.next().value;
+		heightmap[0][ubound] = randomNumber.next().value;
+		heightmap[ubound][0] = randomNumber.next().value;
+		heightmap[ubound][ubound] = randomNumber.next().value;
+
+		let div = ubound;
+		let noiseStrength = 1;
+
+		while(div > 1) {
+			for(let y = 0; y < ubound; y += div) {
+				for(let x = 0; x < ubound; x += div) {
+					const a = heightmap[y][x];
+					const b = heightmap[y][x + div];
+					const c = heightmap[y + div][x];
+					const d = heightmap[y + div][x + div];
+
+					heightmap[y + (div / 2)][x + (div / 2)] = clamp(((a + b + c + d) / 4) + noise(noiseStrength));
+				}
+			}
+
+			for(let y = 0; y < ubound; y += div) {
+				for(let x = 0; x < ubound; x += div) {
+					const a = heightmap[y][x];
+					const b = heightmap[y][x + div];
+					const c = heightmap[y + div][x];
+					const d = heightmap[y + div][x + div];
+					const e = heightmap[y + (div / 2)][x + (div / 2)];
+
+					heightmap[y][x + (div / 2)] = clamp(
+						(y
+							? (heightmap[y - (div / 2)][x + (div / 2)] + a + b + e) / 4
+							: (a + b + e) / 3
+						)
+						+ noise(noiseStrength)
+					);
+
+					heightmap[y + (div / 2)][x] = clamp(
+						(x
+							? (heightmap[y + (div / 2)][x - (div / 2)] + a + e + c) / 4
+							: (a + e + c) / 3
+						)
+						+ noise(noiseStrength)
+					);
+
+					heightmap[y + (div / 2)][x + div] = clamp(
+						(x + div < ubound
+							? (heightmap[y + (div / 2)][x + div + (div / 2)] + b + e + d) / 4
+							: (b + e + d) / 3
+						)
+						+ noise(noiseStrength)
+					);
+
+					heightmap[y + div][x + (div / 2)] = clamp(
+						(y + div < ubound
+							? (heightmap[y + div + (div / 2)][x + (div / 2)] + e + c + d) / 4
+							: (e + c + d) / 3
+						)
+						+ noise(noiseStrength)
+					);
+				}
+			}
+
+			div /= 2;
+			noiseStrength /= 2;
+		}
+
+		return heightmap;
+	}
 }
 
-export default create;
+export default Heightmap;
