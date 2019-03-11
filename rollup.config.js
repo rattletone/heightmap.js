545e2f3e0448a24bb82d192a15b47c330b0e466b
rattletone <rattletone@protonmail.com>
2019-03-11 22:21:09 +0000

    Added minified build output. Changed output file name to lowercase.

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,23 @@
-module.exports = {
-    input: "src/Heightmap.js",
-    output: {
-        file: "dist/Heightmap.js",
-        format: "umd",
-        name: "Heightmap"
+const minify = require("rollup-plugin-babel-minify");
+
+module.exports = [
+    {
+        input: "src/Heightmap.js",
+        output: {
+            file: "dist/heightmap.js",
+            format: "umd",
+            name: "Heightmap"
+        }
+    },
+    {
+        input: "src/Heightmap.js",
+        output: {
+            file: "dist/heightmap.min.js",
+            format: "umd",
+            name: "Heightmap"
+        },
+        plugins: [
+            minify()
+        ]
     }
-};
+];
