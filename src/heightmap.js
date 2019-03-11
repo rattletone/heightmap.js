b99cdbc7cdce9b692a8c57dd938cc118fa3c7a3b
rattletone <rattletone@protonmail.com>
2019-03-11 04:15:22 +0000

    Added seed parameter to heightmap.js create()

--- src/heightmap.js
+++ src/heightmap.js
@@ -2,11 +2,57 @@ function clamp(n) {
 	return Math.min(Math.max(n, 0), 1);
 }
 
-function noise(strength) {
-	return ((Math.random() * (1 - -1)) + -1) * strength;
+function* hash(seed) {
+	let h;
+
+	for(const i of seed) {
+		h = 1779033703 ^ seed.length;
+		h = Math.imul(h ^ i.charCodeAt(0), 3432918353);
+		h = (h << 13) | (h >>> 19);
+	}
+
+	while(true) {
+		h = Math.imul(h ^ (h >>> 16), 2246822507);
+		h = Math.imul(h ^ (h >>> 13), 3266489909);
+
+		yield (h ^= h >>> 16) >>> 0;
+	}
 }
 
-function create(iterations) {
+function* random(aSeed, bSeed, cSeed, dSeed) {
+	let a = aSeed;
+	let b = bSeed;
+	let c = cSeed;
+	let d = dSeed;
+
+	while(true) {
+		let t;
+
+		a >>>= 0;
+		b >>>= 0;
+		c >>>= 0;
+		d >>>= 0;
+		t = (a + b) | 0;
+		a = b ^ (b >>> 9);
+		b = (c + (c << 3)) | 0;
+		c = (c << 21) | (c >> 11);
+		d = (d + 1) | 0;
+		t = (t + d) | 0;
+		c = (c + t) | 0;
+
+		yield (t >>> 0) / 4294967296;
+	}
+}
+
+function create(iterations, seed) {
+	const hashSeed = typeof seed === "string" ? seed : Math.random().toString(36);
+	const randomSeed = hash(hashSeed);
+	const randomNumber = random(randomSeed.next().value, randomSeed.next().value, randomSeed.next().value, randomSeed.next().value);
+
+	function noise(strength) {
+		return ((randomNumber.next().value * (1 - -1)) + -1) * strength;
+	}
+
 	const ubound = Math.pow(2, iterations);
 	const heightmap = [];
 	let i = ubound + 1;
@@ -15,10 +61,10 @@ function create(iterations) {
 		heightmap[i] = [];
 	}
 
-	heightmap[0][0] = Math.random();
-	heightmap[0][ubound] = Math.random();
-	heightmap[ubound][0] = Math.random();
-	heightmap[ubound][ubound] = Math.random();
+	heightmap[0][0] = randomNumber.next().value;
+	heightmap[0][ubound] = randomNumber.next().value;
+	heightmap[ubound][0] = randomNumber.next().value;
+	heightmap[ubound][ubound] = randomNumber.next().value;
 
 	let div = ubound;
 	let noiseStrength = 1;
